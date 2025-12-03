1c6494ecbcc100c0c03ff0e3b8b898224c2a32fe
3LuisF <lmontoya@unal.edu.co>
2025-12-03 14:14:45 -0500

    Desarrollo de taller
    lab ok

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,6 +2,9 @@
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
 
+import pandas as pd
+import os
+
 
 def pregunta_01():
     """
@@ -13,3 +16,59 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    input_file = "files/input/solicitudes_de_credito.csv"
+    output_file = "files/output"
+    
+    df = pd.read_csv(input_file, sep=";", index_col=0)
+    
+    columnas_texto = [
+        "sexo",
+        "tipo_de_emprendimiento",
+        "idea_negocio",
+        "monto_del_credito",
+        "línea_credito",
+    ]
+    
+    for columna in columnas_texto:
+        df[columna] = (
+            df[columna]
+            .str.lower()
+            .str.strip()
+            .str.replace("_", " ", regex=False)
+            .str.replace("-", " ", regex=False)
+            .str.replace(",", "", regex=False)
+            .str.replace(".00", "", regex=False)
+            .str.replace("$", "", regex=False)
+            .str.strip()
+        )
+    
+    df["barrio"] = (
+        df["barrio"]
+        .str.lower()
+        .str.replace("_", " ", regex=False)
+        .str.replace("-", " ", regex=False)
+    )
+    
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    
+    df["monto_del_credito"] = df["monto_del_credito"].astype(float)
+    
+    df["fecha_de_beneficio"] = pd.to_datetime(
+        df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
+    ).combine_first(
+        pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce")
+    )
+    
+    df = df.drop_duplicates()
+    
+    df = df.dropna()
+    
+    if not os.path.exists(output_file):
+        os.makedirs(output_file)
+    
+    df.to_csv(
+        f"{output_file}/solicitudes_de_credito.csv",
+        sep=";",
+        index=False,
+    )
+pregunta_01()
